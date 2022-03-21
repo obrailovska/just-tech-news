f835be5a698794222313f5468334065af42181e0
obrailovska <olabrrai@icloud.com>
2022-03-21 14:50:44 -0400

    feature-password

--- server.js
+++ server.js
@@ -13,8 +13,28 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  // The other thing to notice is the use of {force: false} in the .sync() method. This doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup.
   app.listen(PORT, () => console.log("Now listening"));
 });
 
-// Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server.js to use.
+// const express = require("express");
+// const routes = require("./routes");
+// const sequelize = require("./config/connection");
+// const path = require("path");
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // turn on routes
+// app.use(routes);
+
+// // turn on connection to db and server
+// sequelize.sync({ force: false }).then(() => {
+//   // The other thing to notice is the use of {force: false} in the .sync() method. This doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup.
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
+
+// // Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server.js to use.
