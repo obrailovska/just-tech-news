f1694e845a4bdbd667e8f22b66e65c3c845150d6
obrailovska <olabrrai@icloud.com>
2022-03-22 18:22:43 -0400

    added front end

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -21,26 +35,3 @@ app.use(require("./controllers/"));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
-
-// const express = require("express");
-// const routes = require("./routes");
-// const sequelize = require("./config/connection");
-// const path = require("path");
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // turn on routes
-// app.use(routes);
-
-// // turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//   // The other thing to notice is the use of {force: false} in the .sync() method. This doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup.
-//   app.listen(PORT, () => console.log("Now listening"));
-// });
-
-// // Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server.js to use.
